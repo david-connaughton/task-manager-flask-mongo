5d52f0d9d8d4a6eb499e334adcd973ca68ccdd04
david connaughton <davidconnaughton@davids-MacBook-Pro.local>
2020-10-15 17:40:52 +0100

    Update to Port Config

--- app.py
+++ app.py
@@ -23,5 +23,5 @@ def get_tasks():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+            # port=int(os.environ.get("PORT")),
             debug=True)
